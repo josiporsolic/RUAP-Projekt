652134312a69963b4ec689150ccd509d9e84c86e
Josip Orsolic <josip.orsolic5@gmail.com>
2023-11-08 16:08:30 +0100

    Dodavanje konzolne aplikacije

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_LV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Josip Oršolić");
+        }
+    }
+}
